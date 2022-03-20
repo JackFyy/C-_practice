ed2218c3ccbfb229c88be2a9e41b90d2e0bcc8c8
JackFyy <70005541+JackFyy@users.noreply.github.com>
2022-03-20 18:15:32 +0800

    Add files via upload

---
+++ shared_ptr.cpp
@@ -0,0 +1,128 @@
+#include<iostream>
+using namespace std;
+
+class CStudent
+{
+public:
+    CStudent(){
+
+    }
+
+    void test(){
+        cout<<"CStudent"<<endl;
+        m_nSex = 1;
+    }
+
+private:
+    char* m_pszBuf;
+    int m_nSex;
+
+};
+
+//引用计数
+class CRefCount{
+
+public:
+    
+    friend class CSmartPtr;
+
+    CRefCount(CStudent* pStu){
+        m_Obj = pStu;
+        m_nCount = 1;
+    }
+
+    ~CRefCount(){
+        delete m_Obj;
+        m_Obj = nullptr;
+    }
+
+    void AddRef(){
+        m_nCount++;
+    }
+
+    void Release(){
+        if(--m_nCount == 0){
+            delete this;
+        }
+    }
+
+private:
+    CStudent* m_Obj;
+    int m_nCount;
+
+};
+
+//创建一个类，利用该类的构造和析构(进出作用域自动被编译器调用)的机制来解决资源自动释放的问题
+class CSmartPtr{
+public:
+
+    CSmartPtr(){
+        m_pRef = nullptr;
+    }
+
+    CSmartPtr(CStudent* pStu){
+        m_pRef = new CRefCount(pStu);
+        cout<<"普通构造函数"<<endl;
+    }
+
+    ~CSmartPtr(){
+        m_pRef->Release();
+    }
+
+    CSmartPtr(CSmartPtr& obj){
+        cout<<"拷贝构造"<<endl;
+        m_pRef = obj.m_pRef;
+        m_pRef->AddRef();
+    }
+
+    CSmartPtr& operator=(CSmartPtr& obj){
+        cout<<"运算符重载"<<endl;
+        if(m_pRef == obj.m_pRef){
+            return *this;
+        }
+
+        if(m_pRef!=nullptr){
+            m_pRef->Release();
+        }
+
+        m_pRef = obj.m_pRef;
+        m_pRef->AddRef();
+
+        return *this;
+        
+    }
+  
+    //需要想办法让CSmartPtr像一个指针(运算符重载)
+    CStudent* operator->(){
+        return m_pRef->m_Obj;
+    }
+    CStudent** operator&(){
+        return &m_pRef->m_Obj;
+    }
+
+    CStudent& operator*(){
+        return *m_pRef->m_Obj;
+    }
+    operator CStudent*(){
+        return m_pRef->m_Obj;
+    }
+
+
+private:
+    CRefCount* m_pRef; //将资源放入智能指针类中，管理起来
+
+};
+
+int main(){
+
+    //结合引用计数和写时拷贝
+    CStudent* pStu = new CStudent();
+
+    CSmartPtr sp1(pStu);
+    CSmartPtr sp2(sp1); //调拷贝构造
+
+    sp2=sp1; //调运算符重载
+    
+
+    return 0;
+}
